39d92d38a664508b31b1caa36760345c863f3914
Thomi Richards <thomi.richards@canonical.com>
2017-01-25 09:19:04 +1300

    Add setup.py and requirements files.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2017 Canonical Ltd.  This software is licensed under the
+# GNU Lesser General Public License version 3 (see the file LICENSE).
+
+import pkg_resources
+from setuptools import setup
+
+
+VERSION = '0.1'
+
+
+def parse_requirements_file(path):
+    """Parse a requirements file, return a list of requirements as strings."""
+    with open(path, 'r') as rfile:
+        deps = []
+        for requirement in rfile.readlines():
+            if requirement.isspace():
+                continue
+            deps.append(
+                pkg_resources.Requirement.parse(requirement).project_name)
+        return deps
+
+
+setup(
+    name='acceptable',
+    version=VERSION,
+    description='API version negotiation for flask-based web services.',
+    author='Canonical Online Services',
+    author_email='Online Services <online-services@lists.canonical.com>',
+    license='LGPLv3',
+    py_module=['acceptable'],
+    install_requires=parse_requirements_file('requirements.txt'),
+    test_suite='test_acceptable',
+    tests_require=parse_requirements_file('requirements-dev.txt')
+)
