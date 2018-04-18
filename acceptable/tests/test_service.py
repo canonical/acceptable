4e22784c851b9ac1ee9590f690c42d4f31e1612d
bloodearnest <bloodearnest@gmail.com>
2018-04-18 17:37:57 +0100

    Remove support for content negotiation.
    This removes a lot of complexity from AcceptableService, as paves the
    way for gathering metadata by import rather than AST.
    
    This is an internal refactor, and preserves the existing API, but does
    alter behaviour: you can no only have one view per url/methods pair.

--- acceptable/tests/test_service.py
+++ acceptable/tests/test_service.py
@@ -1,22 +1,16 @@
 # Copyright 2017 Canonical Ltd.  This software is licensed under the
 # GNU Lesser General Public License version 3 (see the file LICENSE).
 
-from operator import methodcaller
-
 from fixtures import Fixture
 from flask import Flask
-from testscenarios import TestWithScenarios
 from testtools import TestCase
 from testtools.matchers import (
-    Contains,
     Equals,
     Matcher,
 )
 
 from acceptable._service import (
     AcceptableService,
-    EndpointMap,
-    parse_accept_headers,
 )
 
 
@@ -25,92 +19,29 @@ class AcceptableServiceTestCase(TestCase):
     def test_raises_TypeError_on_construction(self):
         self.assertRaisesRegex(
             TypeError,
-            "vendor must be a string, not object",
+            "name must be a string, not object",
             AcceptableService,
-            object(), object()
-        )
-
-    def test_raises_ValueError_on_construction(self):
-        self.assertRaisesRegex(
-            ValueError,
-            "vendor identifier must consist of alphanumeric "
-            "characters only.",
-            AcceptableService,
-            "vendor.foo", object()
+            object(),
         )
 
     def test_can_register_url_route(self):
         def view():
             return "test view", 200
 
-        app = Flask(__name__)
-        service = AcceptableService('vendor', app)
+        service = AcceptableService('service')
         api = service.api('/foo', 'foo_api')
-        api.register_view('1.0', None, view)
+        api.register_view('1.0', view)
+
+        app = Flask(__name__)
+        service.bind(app)
 
         client = app.test_client()
         resp = client.get('/foo')
 
         self.assertThat(resp, IsResponse('test view'))
 
-    def test_can_register_url_route_with_two_phase_registration(self):
-        def view():
-            return "test view", 200
 
-        service = AcceptableService('vendor')
-        api = service.api('/foo', 'foo_api')
-        api.register_view('1.0', None, view)
-
-        app = Flask(__name__)
-        service.initialise(app)
-
-        client = app.test_client()
-        resp = client.get('/foo')
-
-        self.assertThat(resp, IsResponse('test view'))
-
-    def test_can_rebind_api_to_second_flask_application(self):
-        def view():
-            return "test view", 200
-
-        service = AcceptableService('vendor')
-        api = service.api('/foo', 'foo_api')
-        api.register_view('1.0', None, view)
-
-        app1 = Flask(__name__)
-        app2 = Flask(__name__)
-        service.initialise(app1)
-
-        client = app1.test_client()
-        resp = client.get('/foo')
-
-        self.assertThat(resp, IsResponse('test view'))
-
-        service.initialise(app2)
-        client = app2.test_client()
-        resp = client.get('/foo')
-
-        self.assertThat(resp, IsResponse('test view'))
-
-    def test_rebinding_to_existing_application_is_a_noop(self):
-        def view():
-            return "test view", 200
-
-        service = AcceptableService('vendor')
-        api = service.api('/foo', 'foo_api')
-        api.register_view('1.0', None, view)
-
-        app1 = Flask(__name__)
-        service.initialise(app1)
-        service.initialise(app1)
-
-        client = app1.test_client()
-        resp = client.get('/foo')
-
-        self.assertThat(resp, IsResponse('test view'))
-
-
-class SimpleAPIServiceFixture(Fixture):
+class ServiceFixture(Fixture):
     """A reusable fixture that sets up several API endpoints.
 
     This fixture creates a simple set of API endpoints with a mix of different
@@ -119,106 +50,24 @@ class SimpleAPIServiceFixture(Fixture):
     """
 
     def _setUp(self):
-        self.flask_app = Flask(__name__)
-        self.service = AcceptableService('vendor', self.flask_app)
-
-        # The /foo API is POST only, and contains three different versioned
-        # endpoints:
+        self.service = AcceptableService('service')
         foo_api = self.service.api('/foo', 'foo_api', methods=['POST'])
-        foo_api.register_view('1.0', None, self.foo_v10)
-        foo_api.register_view('1.1', None, self.foo_v11)
-        foo_api.register_view('1.3', None, self.foo_v13)
-        foo_api.register_view('1.5', None, self.foo_v15)
+        foo_api.register_view('1.0', self.foo)
 
-        # The /flagged API is GET only, and has some API flags set:
-        flagged_api = self.service.api('/flagged', 'flagged', methods=['GET'])
-        flagged_api.register_view('1.3', None, self.flagged_v13)
-        flagged_api.register_view('1.4', 'feature1', self.flagged_v14_feature1)
-        flagged_api.register_view('1.4', 'feature2', self.flagged_v14_feature2)
+    def bind(self, app=None):
+        if app is None:
+            app = Flask(__name__)
+        self.service.bind(app)
+        return app
 
-    def foo_v10(self):
-        return "Foo version 1.0", 200
-
-    def foo_v11(self):
-        return "Foo version 1.1", 200
-
-    def foo_v13(self):
-        return "Foo version 1.3", 200
-
-    def foo_v15(self):
-        return "Foo version 1.5", 200
-
-    def flagged_v13(self):
-        return "Flagged version 1.3", 200
-
-    def flagged_v14_feature1(self):
-        return "Flagged version 1.4 with feature1", 200
-
-    def flagged_v14_feature2(self):
-        return "Flagged version 1.4 with feature2", 200
+    def foo(self):
+        return "foo", 200
 
 
 class AcceptableAPITestCase(TestCase):
 
-    def test_default_view_is_latest_version(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-        client = fixture.flask_app.test_client()
-        resp = client.post('/foo')
-
-        self.assertThat(resp, IsResponse("Foo version 1.5"))
-
-    def test_can_select_specific_version(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-        client = fixture.flask_app.test_client()
-        resp = client.post(
-            '/foo',
-            headers={'Accept': 'application/vnd.vendor.1.1'})
-
-        self.assertThat(resp, IsResponse("Foo version 1.1"))
-
-    def test_versions_are_downgraded(self):
-        # Ask for version 1.2, but since it doesn't exist we'll
-        # get the next oldest version instead.
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-        client = fixture.flask_app.test_client()
-        resp = client.post(
-            '/foo',
-            headers={'Accept': 'application/vnd.vendor.1.2'})
-
-        self.assertThat(resp, IsResponse("Foo version 1.1"))
-
-    def test_get_unflagged_view_by_default(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-        client = fixture.flask_app.test_client()
-        resp = client.get('/flagged')
-
-        self.assertThat(resp, IsResponse("Flagged version 1.3"))
-
-    def test_can_select_flagged_view(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-        client = fixture.flask_app.test_client()
-        resp = client.get(
-            '/flagged',
-            headers={'Accept': 'application/vnd.vendor.1.4+feature1'})
-
-        self.assertThat(resp, IsResponse("Flagged version 1.4 with feature1"))
-
-    def test_no_acceptable_version(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-        client = fixture.flask_app.test_client()
-        resp = client.get(
-            '/flagged',
-            headers={'Accept': 'application/vnd.vendor.1.0+foo'})
-
-        self.assertThat(
-            resp,
-            IsResponse(
-                Contains(
-                    "Could not find view for version '1.0' and flag 'foo'"),
-                406))
-
     def test_view_decorator_works(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
+        fixture = self.useFixture(ServiceFixture())
 
         new_api = fixture.service.api('/new', 'blah')
 
@@ -226,263 +75,65 @@ class AcceptableAPITestCase(TestCase):
         def new_view():
             return "new view", 200
 
-        client = fixture.flask_app.test_client()
+        app = fixture.bind()
+
+        client = app.test_client()
         resp = client.get('/new')
 
         self.assertThat(resp, IsResponse("new view"))
-        view = fixture.flask_app.view_functions['blah']
-        self.assertEqual(view.__name__, 'blah')
+        view = app.view_functions['blah']
+        self.assertEqual(view.__name__, 'new_view')
 
     def test_can_still_call_view_directly(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
+        fixture = self.useFixture(ServiceFixture())
 
         new_api = fixture.service.api('/new', 'namegoeshere')
 
         @new_api.view(introduced_at='1.0')
         def new_view():
             return "new view", 200
-        with fixture.flask_app.test_request_context('/new'):
+
+        app = fixture.bind()
+        with app.test_request_context('/new'):
             content, status = new_view()
+
         self.assertEqual(content, "new view")
         self.assertEqual(status, 200)
 
-    def test_can_reuse_url_with_different_method(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
+    def test_cannot_duplicate_name(self):
+        fixture = self.useFixture(ServiceFixture())
 
-        # /foo already exists as a POST endpoint, we should be able to
-        # create another /foo API with a GET endpoint.
-        foo_get_api = fixture.service.api('/foo', 'foo', methods=['GET'])
-
-        @foo_get_api.view(introduced_at='1.0')
-        def get_foo():
-            return "Foo GET API"
-
-        client = fixture.flask_app.test_client()
-        resp_get = client.get('/foo')
-
-        self.assertThat(resp_get, IsResponse("Foo GET API"))
-
-    def test_view_attributes_are_preserved(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-
-        new_api = fixture.service.api('/new', 'new')
-
-        def my_decorator(fn):
-            fn.foo = 'bar'
-            return fn
-
-        @new_api.view(introduced_at='1.0')
-        @my_decorator
-        def new_view():
-            return "new view", 200
-
-        self.assertEqual(new_view.foo, 'bar')
-
-    def test_cannot_duplicate_api_names(self):
-        fixture = self.useFixture(SimpleAPIServiceFixture())
-
-        fixture.service.api('/new', 'some name here')
-        e = self.assertRaises(
-            ValueError,
+        self.assertRaises(
+            AssertionError,
             fixture.service.api,
-            '/another',
-            'some name here'
-        )
-        self.assertEqual(
-            "The name 'some name here' has already been registered for an "
-            "API.", str(e))
-
-    def test_can_expose_same_view_in_multiple_apis(self):
-        flask_app = Flask(__name__)
-        service = AcceptableService('vendor', flask_app)
-
-        api1 = service.api('/foo1', 'foo1')
-        api2 = service.api('/foo2', 'foo2')
-
-        @api1.view(introduced_at='1.0')
-        @api2.view(introduced_at='1.0')
-        def get_foo():
-            return "Foo API"
-
-        client = flask_app.test_client()
-
-        resp1 = client.get('/foo1')
-        resp2 = client.get('/foo2')
-
-        self.assertThat(resp1, IsResponse("Foo API"))
-        self.assertThat(resp2, IsResponse("Foo API"))
-
-
-
-class EndpointMapTestCase(TestCase):
-
-    def test_simple_match(self):
-        # Test an exact match without flags
-        m = EndpointMap()
-        view = object()
-
-        m.add_view('1.0', None, view)
-
-        self.assertEqual(view, m.get_view('1.0'))
-
-    def test_version_downgrade(self):
-        # If we can't satisfy the version requirement we'll return an
-        # older view.
-        m = EndpointMap()
-        view = object()
-
-        m.add_view('1.2', None, view)
-
-        self.assertEqual(view, m.get_view('1.3'))
-
-    def test_flagged_view(self):
-        # Test an exact match with flags:
-        m = EndpointMap()
-        normal_view = object()
-        flagged_view = object()
-
-        m.add_view('1.0', None, normal_view)
-        m.add_view('1.0', 'flag', flagged_view)
-
-        self.assertEqual(normal_view, m.get_view('1.0'))
-        self.assertEqual(flagged_view, m.get_view('1.0', 'flag'))
-
-    def test_flagged_downgrade(self):
-        # If we can't satisfy the flag request we'll ignore it:
-        m = EndpointMap()
-        normal_view = object()
-
-        m.add_view('1.0', None, normal_view)
-
-        self.assertEqual(normal_view, m.get_view('1.0', 'flag'))
-
-    def test_does_not_version_upgrade(self):
-        # if we can't satisfy the version request we won't upgrade to
-        # an older version
-        m = EndpointMap()
-        view = object()
-
-        m.add_view('1.2', None, view)
-
-        self.assertEqual(None, m.get_view('1.1'))
-
-    def test_version_downgrade_is_smallest_increment(self):
-        # If we can't satisfy the exact version requested, give the
-        # client the smallest decrement in version possible.
-        m = EndpointMap()
-        view11 = object()
-        view12 = object()
-        view13 = object()
-
-        m.add_view('1.1', None, view11)
-        m.add_view('1.2', None, view12)
-        m.add_view('1.3', None, view13)
-
-        self.assertEqual(view13, m.get_view('1.4'))
-
-
-class EndpointMapTypeCheckingTests(TestWithScenarios):
-
-    scenarios = [
-        ('bad version type', {
-            'args': (True, None, None),
-            'exception': TypeError,
-            'expected_error': "Version must be a string, not bool",
-        }),
-        ('bad flag type', {
-            'args': ('1.0', object(), None),
-            'exception': TypeError,
-            'expected_error': "Flag must be a string or None, not object",
-        }),
-        ('version with too many components', {
-            'args': ('1.0.0', None, None),
-            'exception': ValueError,
-            'expected_error': "Version must be in the format "
-                              "<major>.<minor>",
-        }),
-        ('bad major version', {
-            'args': ('one.2', None, None),
-            'exception': ValueError,
-            'expected_error': "Major version number is not an integer",
-        }),
-        ('bad minor version', {
-            'args': ('1.two', None, None),
-            'exception': ValueError,
-            'expected_error': "Minor version number is not an integer",
-        }),
-    ]
-
-    def test_add_view_type_checking(self):
-        m = EndpointMap()
-        self.assertRaisesRegex(
-            self.exception,
-            self.expected_error,
-            methodcaller('add_view', *self.args),
-            m
+            '/bar',
+            'foo_api',
         )
 
-    def test_get_view_type_checking(self):
-        # get view doesn't accept the view parameter, so trim it from the
-        # argument list:
-        args = self.args[:-1]
-        m = EndpointMap()
-        self.assertRaisesRegex(
-            self.exception,
-            self.expected_error,
-            methodcaller('get_view', *args),
-            m
+    def test_cannot_duplicate_url_and_method(self):
+        fixture = self.useFixture(ServiceFixture())
+        self.assertRaises(
+            AssertionError,
+            fixture.service.api,
+            '/foo',
+            'bar',
+            methods=['POST'],
         )
 
+    def test_can_duplicate_url_different_method(self):
+        fixture = self.useFixture(ServiceFixture())
 
-class AcceptHeaderParseTests(TestWithScenarios):
+        alt_api = fixture.service.api('/foo', 'foo_alt', methods=['GET'])
 
-    scenarios = [
-        ('No match', {
-            'vendor': '',
-            'headers': ['*/*'],
-            'expected': (None, None),
-        }),
-        ('Mismatched vendor', {
-            'vendor': 'foo',
-            'headers': ['application/vnd.bar.1.2'],
-            'expected': (None, None),
-        }),
-        ('Normal mimetype', {
-            'vendor': 'foo',
-            'headers': ['text/html'],
-            'expected': (None, None),
-        }),
-        ('invalid version format', {
-            'vendor': 'foo',
-            'headers': ['application/vnd.foo.version1'],
-            'expected': (None, None),
-        }),
-        ('Version with no flag', {
-            'vendor': 'foo',
-            'headers': ['application/vnd.foo.1.2'],
-            'expected': ('1.2', None),
-        }),
-        ('Version with flag', {
-            'vendor': 'foo',
-            'headers': ['application/vnd.foo.1.3+feature1'],
-            'expected': ('1.3', 'feature1'),
-        }),
-        ('match after normal mimetype', {
-            'vendor': 'foo',
-            'headers': ['text/html', 'application/vnd.foo.1.3+feature1'],
-            'expected': ('1.3', 'feature1'),
-        }),
-        ('match after vendor mismatch mimetype', {
-            'vendor': 'foo',
-            'headers': ['application/vnd.bar.1.2',
-                        'application/vnd.foo.1.3+feature1'],
-            'expected': ('1.3', 'feature1'),
-        }),
-    ]
+        @alt_api.view(introduced_at='1.0')
+        def foo_alt():
+            return "alt foo", 200
 
-    def test_header_parsing(self):
-        observed = parse_accept_headers(self.vendor, self.headers)
-        self.assertEqual(observed, self.expected)
+        app = fixture.bind()
+        client = app.test_client()
+        resp = client.get('/foo')
+
+        self.assertThat(resp, IsResponse("alt foo"))
 
 
 class IsResponse(Matcher):
