ad05dc194c6941cbfa7c3394ed424b5a93e2df46
Simon Davy <simon.davy@canonical.com>
2018-07-05 14:14:59 +0000

    release 0.15

---
+++ CHANGELOG.rst
@@ -0,0 +1,6 @@
+
+Version 0.15
+
+ * Sort changelogs properly in docs
+ * Add global changelog
+ * Support undocumented apis
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import pkg_resources
 from setuptools import setup
 
 
-VERSION = '0.14'
+VERSION = '0.15'
 
 
 def parse_requirements_file(path):
