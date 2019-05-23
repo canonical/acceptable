bbedb364c677ba2811f635d5c156c248bfc527bd
Tony Simpson <tony.simpson@canonical.com>
2019-05-23 12:35:43 +0100

    Added double generation subcommand
    Reads in metadata from stdin or from a file like api.json
    Currently creates doubles based on ServiceMock.

---
+++ acceptable/tests/test_generate_doubles.py
@@ -0,0 +1,15 @@
+from future import standard_library
+standard_library.install_aliases()
+
+import json
+from io import StringIO
+
+from acceptable import generate_doubles
+import testtools
+
+class GenerateDoublesTests(testtools.TestCase):
+    def test_generate_service_mock_doubles_from_example(self):
+        stream = StringIO()
+        metadata = json.load(open('examples/current_api.json'))
+        generate_doubles.generate_service_mock_doubles(metadata, stream=stream)
+        self.assertIn('foo_1_0 = service_mock', stream.getvalue())
