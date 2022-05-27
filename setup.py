6bde79305787512c0edefad36a0368f4c51cb5cf
Wouter van Bommel <wouter.bommel@canonical.com>
2022-05-27 13:27:11 +1000

    Reduce supported python versions to current ones
    Only support python 3.8 and python 3.10.
    This also opens up the support for modern versions of Flask & Jinja2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import find_packages, setup
 
 
-VERSION = '0.32'
+VERSION = '0.33'
 
 
 setup(
@@ -23,12 +23,11 @@ setup(
     install_requires=[
         'jsonschema',
         'pyyaml',
+        'Jinja2',
     ],
     extras_require=dict(
         flask=[
-            'Flask<2.0',
-            'Jinja2<3.0,>=2.10.1',
-            'MarkupSafe<2.1',
+            'Flask',
         ],
         django=[
             'django>=2.1,<3',
