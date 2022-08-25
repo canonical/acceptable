09dfc826fd5bdb387ee023854defb5b12547e53f
David Seaward <david.seaward@canonical.com>
2022-08-25 15:03:33 +0100

    Hide _to_dict from other modules.
    Signed-off-by: David Seaward <david.seaward@canonical.com>

--- acceptable/openapi_exporter.py
+++ acceptable/openapi_exporter.py
@@ -8,19 +8,19 @@ from typing import Any
 import yaml
 
 
-def to_dict(source: Any):
+def _to_dict(source: Any):
 
-    if hasattr(source, "to_dict"):
-        return source.to_dict()
+    if hasattr(source, "_to_dict"):
+        return source._to_dict()  # noqa
     elif type(source) == dict:
         source_dict = {}
         for key, value in source.items():
-            source_dict[key] = to_dict(value)
+            source_dict[key] = _to_dict(value)
         return source_dict
     elif type(source) == list:
         source_list = []
         for item in source:
-            source_list.append(to_dict(item))
+            source_list.append(_to_dict(item))
         return source_list
     else:
         return source
@@ -29,7 +29,7 @@ def to_dict(source: Any):
 def dump_oas(schema, stream):
 
     return yaml.safe_dump(
-        to_dict(schema),
+        _to_dict(schema),
         stream,
         default_flow_style=False,
         encoding=None,
