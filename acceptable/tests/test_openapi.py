7adade70993021661dd26fc523da9f64d24434a3
David Seaward <david.seaward@canonical.com>
2022-09-14 19:08:48 +0100

    Add tests for new helper methods.
    Signed-off-by: David Seaward <david.seaward@canonical.com>

--- acceptable/tests/test_openapi.py
+++ acceptable/tests/test_openapi.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 import testtools
 
 from acceptable import openapi
-from acceptable._service import APIMetadata
+from acceptable._service import APIMetadata, AcceptableAPI, AcceptableService
 
 
 @dataclass
@@ -50,6 +50,83 @@ class ToDictTests(testtools.TestCase):
     def test_convert_str_returns_same_value():
         result = openapi._to_dict("beeblebrox")
         assert "beeblebrox" == result
+
+
+class TidyStringTests(testtools.TestCase):
+    @staticmethod
+    def test_none():
+        assert "None" == openapi.tidy_string(None)
+
+    @staticmethod
+    def test_empty():
+        assert "" == openapi.tidy_string("")
+
+    @staticmethod
+    def test_tidy_returns_tidy():
+        assert "simple" == openapi.tidy_string("simple")
+
+    @staticmethod
+    def test_collapse_two_spaces():
+        assert "hello world" == openapi.tidy_string("hello  world")
+
+    @staticmethod
+    def test_collapse_three_spaces():
+        assert "hello world" == openapi.tidy_string("hello   world")
+
+    @staticmethod
+    def test_strip_trailing_spaces():
+        assert "hello world" == openapi.tidy_string(" hello world ")
+
+
+class GetHeadTests(testtools.TestCase):
+    def test_empty(self):
+        self.assertIsNone(openapi.get_head_of_single_item_list([], "item"))
+
+    @staticmethod
+    def test_single_item_list():
+        assert "HEAD" == openapi.get_head_of_single_item_list(["HEAD"], "item")
+
+    def test_multiple_item_list_returns_none(self):
+        self.assertIsNone(
+            openapi.get_head_of_single_item_list(["HEAD", "TAIL"], "item")
+        )
+
+
+class EndpointToOperationTests(testtools.TestCase):
+    @staticmethod
+    def test_blank():
+        endpoint = AcceptableAPI(
+            introduced_at=1,
+            name="test name",  # maps to operation.operation_id
+            service=AcceptableService(name="test service"),
+            url="https://test.example",
+        )
+        operation = openapi.convert_endpoint_to_operation(endpoint)
+        assert "None" == operation.description
+        assert "test name" == operation.operation_id
+        assert operation.summary is None
+        assert 1 == len(operation.tags)
+        assert "none" == operation.tags[0]
+
+    @staticmethod
+    def test_populated():
+        endpoint = AcceptableAPI(
+            introduced_at=1,
+            name="test name",  # maps to operation.operation_id
+            service=AcceptableService(
+                name="test service",
+                group="test group",  # maps to operation.tags
+            ),
+            title="test title",  # maps to operation.summary
+            url="https://test.example",
+        )
+        endpoint.docs = "test docs"  # maps to operation.description
+        operation = openapi.convert_endpoint_to_operation(endpoint)
+        assert "test docs" == operation.description
+        assert "test name" == operation.operation_id
+        assert "test title" == operation.summary
+        assert 1 == len(operation.tags)
+        assert "test group" == operation.tags[0]
 
 
 class OpenApiTests(testtools.TestCase):
